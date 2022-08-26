808847647eeba5ef12f225ff14ed91f565663312
Karsei <karseidev@gmail.com>
2022-08-26 23:59:09 +0900

    feat: cra 로 변경

--- static/weekly-report/src/App.tsx
+++ static/weekly-report/src/App.tsx
@@ -1,19 +1,18 @@
-import React, {useEffect, useState} from 'react'
-import Button from '@atlaskit/button'
-import './App.css'
-import WeeklySearchForm from './components/WeeklySearchForm'
+import React from 'react';
+import './App.css';
+import WeeklySearchForm from './components/WeeklySearchForm';
 
 function App() {
-    // const [data, setData] = useState(null);
-    //
-    // useEffect(() => {
-    //     invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-    // }, []);
-    return (
-        <div className="App">
-            <WeeklySearchForm />
-        </div>
-    )
+  // const [data, setData] = useState(null);
+  //
+  // useEffect(() => {
+  //     invoke('getText', { example: 'my-invoke-variable' }).then(setData);
+  // }, []);
+  return (
+    <div className="">
+      <WeeklySearchForm />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- static/weekly-report/src/components/WeeklySearchForm.tsx
+++ static/weekly-report/src/components/WeeklySearchForm.tsx
@@ -1,35 +1,30 @@
-import React from 'react'
-import Button from '@atlaskit/button'
-import { DatePicker } from '@atlaskit/datetime-picker'
-import Form, { Field, FormFooter, HelperMessage } from '@atlaskit/form'
+import React from 'react';
+import Button from '@atlaskit/button';
+import { DatePicker } from '@atlaskit/datetime-picker';
+import Form, { Field, FormFooter, HelperMessage } from '@atlaskit/form';
 
 const WeeklySearchForm = function () {
-    return (
-        <Form
-            onSubmit={(formState: unknown) => console.log('form submitted', formState)}
-        >
-            {({ formProps }) => (
-                <form {...formProps}>
-                    <Field
-                        name={"startDate"}
-                        label={"시작 날짜"}
-                        isRequired={true}
-                        defaultValue={""}
-                    >
-                        {({ fieldProps }) => (
-                            <>
-                                <DatePicker {...fieldProps} />
-                                <HelperMessage>Help or instruction text goes here</HelperMessage>
-                            </>
-                        )}
-                    </Field>
-                    <FormFooter>
-                        <Button type="submit" appearance={"primary"}>조회</Button>
-                    </FormFooter>
-                </form>
+  return (
+    <Form onSubmit={(formState: unknown) => console.log('form submitted', formState)}>
+      {({ formProps }) => (
+        <form {...formProps}>
+          <Field name={'startDate'} label={'시작 날짜'} isRequired={true} defaultValue={''}>
+            {({ fieldProps }) => (
+              <>
+                <DatePicker {...fieldProps} />
+                <HelperMessage>Help or instruction text goes here</HelperMessage>
+              </>
             )}
-        </Form>
-    )
-}
+          </Field>
+          <FormFooter>
+            <Button type="submit" appearance={'primary'}>
+              조회
+            </Button>
+          </FormFooter>
+        </form>
+      )}
+    </Form>
+  );
+};
 
-export default WeeklySearchForm
+export default WeeklySearchForm;
